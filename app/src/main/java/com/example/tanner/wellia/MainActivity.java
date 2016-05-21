eaa8a8210f7220bce92ba2b7836d309374134411
candace <candacekb42@gmail.com>
2016-05-21 15:51:40 -0400

    Interface updates

--- app/src/main/java/com/example/tanner/wellia/MainActivity.java
+++ app/src/main/java/com/example/tanner/wellia/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.tanner.wellia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Array adapter using type string_array
+        Spinner serviceTypeSpinner = (Spinner) findViewById(R.id.type);
+        ArrayAdapter<CharSequence> serviceTypeAdapter = ArrayAdapter.createFromResource(
+                this, R.array.service_type, android.R.layout.simple_spinner_item);
+        serviceTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply adapter to spinner
+        serviceTypeSpinner.setAdapter(serviceTypeAdapter);
+
+
+        //Array adapter using distance string_array
+        Spinner distanceSpinner = (Spinner) findViewById(R.id.distance);
+        ArrayAdapter<CharSequence> distanceAdapter = ArrayAdapter.createFromResource(
+                this, R.array.distance, android.R.layout.simple_spinner_item);
+        serviceTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply adapter to spinner
+        distanceSpinner.setAdapter(distanceAdapter);
+
+        final EditText zip = (EditText) findViewById(R.id.zip_code);
+
+        ToggleButton locToggle = (ToggleButton) findViewById(R.id.use_loc);
+        locToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    zip.setVisibility(View.VISIBLE);
+                }
+                else {
+                    zip.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+
     }
+
 }

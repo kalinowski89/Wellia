072499c240abea7412bb889173e2613fb7caa49f
kalinowski89 <alkal89@yahoo.com>
2016-05-21 16:03:15 -0400

    parsing json into array updated

--- app/src/main/java/com/example/tanner/wellia/dataParse.java
+++ app/src/main/java/com/example/tanner/wellia/dataParse.java
@@ -4,13 +4,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 5/21/2016.
@@ -18,33 +13,32 @@ import java.net.URL;
 public class dataParse {
 
     public JSONArray parseData() throws IOException, JSONException {
-        URL url = new URL("http://dyn.tdbtech.co/get_data.php");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-        InputStream stream = connection.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String json_obj = new dataProcessLayer().get_data();
 
-        StringBuffer buffer = new StringBuffer();
-
-        String finalJson = buffer.toString();
-
-        JSONObject pObject = new JSONObject(finalJson);
+        JSONObject pObject = new JSONObject(json_obj);
         JSONArray pArray = pObject.getJSONArray("data");
 
-        /*
-        JSONObject finalObject = pArray.getJSONObject(0);
+/*
+        ArrayList<JSONObject> finalObject = new ArrayList<JSONObject>();
+        for(int i=0; i<pArray.length(); i++) {
 
-        String id = finalObject.getString("PROGRAM_ID");
-        String name = finalObject.getString("NAME1");
-        String facility_desc = finalObject.getString("NAME2");
-        String address = finalObject.getString("STREET1") + " " + finalObject.getString("STREET2");
-        String zip = finalObject.getString("ZIP");
-        String phone = finalObject.getString("PHONE") + " " + finalObject.getString("INTAKE1") + " " + finalObject.getString("INTAKE2");
-        String facility_url = finalObject.getString("URL");
-        double latitude = finalObject.getDouble("LAT");
-        double longitude = finalObject.getDouble("LNG");
-        String facility_type = finalObject.getString("TYPE_FACILITY");
-        */
+            finalObject.add(pArray.getJSONObject(i));
+
+            String id = finalObject.getString("PROGRAM_ID");
+            String name = finalObject.getString("NAME1");
+            String facility_desc = finalObject.getString("NAME2");
+            String address = finalObject.getString("STREET1") + " " + finalObject.getString("STREET2");
+            String zip = finalObject.getString("ZIP");
+            String phone = finalObject.getString("PHONE") + " " + finalObject.getString("INTAKE1") + " " + finalObject.getString("INTAKE2");
+            String facility_url = finalObject.getString("URL");
+            double latitude = finalObject.getDouble("LAT");
+            double longitude = finalObject.getDouble("LNG");
+            String facility_type = finalObject.getString("TYPE_FACILITY");
+
+            // can use finalObject.get(i) to access the data within each JSONObject
+        }
+*/
+
 
         return pArray;
     }

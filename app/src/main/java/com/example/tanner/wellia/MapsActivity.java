a8df3efb772c0cd63f4704b418bc57bbf83381fd
Tanner Black <tannerdblack@gmail.com>
2016-05-21 20:28:07 -0400

    JSON to Map Implemented, FTW.

--- app/src/main/java/com/example/tanner/wellia/MapsActivity.java
+++ app/src/main/java/com/example/tanner/wellia/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.tanner.wellia;
 
+import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,11 +11,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +33,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         mMap = googleMap;
 
         double user_lat=50.0,user_lng=50.0;
@@ -45,15 +46,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // center the camera around the user's location
         LatLng user_location = new LatLng(user_lat, user_lng);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(user_location));
+        JSONArray ja=null;
+        try {
+            ja = new dataParse().parseData();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
 
         // loop through the data and set a marker at each lat and long
-        for (int i = 0; i < locations.size(); i++) {
-            // create a new marker
-            LatLng new_loc = new LatLng(locations.get(i).getLat(), locations.get(i).getLng());
-            mMap.addMarker(new MarkerOptions().position(new_loc).title("New Location"));
-            
+        if (ja != null) {
+            for (int i = 0; i < ja.length(); i++) {
+                // create a new marker
+                try {
+                    JSONObject tmp = (JSONObject)(ja.get(i));
+                    System.out.println();
+                    LatLng new_loc = new LatLng(tmp.getDouble("LAT"),tmp.getDouble("LNG"));
+                    mMap.addMarker(new MarkerOptions().position(new_loc).title(tmp.getString("NAME1")));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
         }
-        System.out.println("HERE");
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
